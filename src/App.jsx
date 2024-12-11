772f3aa07111cf24c9dbdd248a41d6105e86da7f
LeandroReichardt <163359722+LeandroReichardt@users.noreply.github.com>
2024-12-11 00:07:01 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import GaleriadeImagens from './components/GaleriadeImagens'
+
+function App() {
+
+  return (
+    <>
+    <GaleriadeImagens/>
+    </>
+  )
+}
+
+export default App
